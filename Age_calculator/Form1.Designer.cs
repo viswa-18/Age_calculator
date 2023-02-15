a1b5a215925ceb906181515a51097a9dc8a0d5f7
viswa-18 <viswadarsini29@gmail.com>
2023-02-15 23:19:41 +0530

    First commit

---
+++ Age_calculator/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace Age_calculator
+{
+    partial class frmAgecalculator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbldate = new System.Windows.Forms.Label();
+            this.lblBirthMontth = new System.Windows.Forms.Label();
+            this.lblBirthyear = new System.Windows.Forms.Label();
+            this.txtboxmonth = new System.Windows.Forms.TextBox();
+            this.txtboxyear = new System.Windows.Forms.TextBox();
+            this.txtboxage = new System.Windows.Forms.TextBox();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.txtboxdate = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lbldate
+            // 
+            this.lbldate.AutoSize = true;
+            this.lbldate.Location = new System.Drawing.Point(218, 123);
+            this.lbldate.Name = "lbldate";
+            this.lbldate.Size = new System.Drawing.Size(125, 16);
+            this.lbldate.TabIndex = 0;
+            this.lbldate.Text = "Enter your birthdate:";
+            // 
+            // lblBirthMontth
+            // 
+            this.lblBirthMontth.AutoSize = true;
+            this.lblBirthMontth.Location = new System.Drawing.Point(218, 186);
+            this.lblBirthMontth.Name = "lblBirthMontth";
+            this.lblBirthMontth.Size = new System.Drawing.Size(134, 16);
+            this.lblBirthMontth.TabIndex = 1;
+            this.lblBirthMontth.Text = "Enter your birthmonth:";
+            // 
+            // lblBirthyear
+            // 
+            this.lblBirthyear.AutoSize = true;
+            this.lblBirthyear.Location = new System.Drawing.Point(218, 245);
+            this.lblBirthyear.Name = "lblBirthyear";
+            this.lblBirthyear.Size = new System.Drawing.Size(125, 16);
+            this.lblBirthyear.TabIndex = 2;
+            this.lblBirthyear.Text = "Enter your birthyear:";
+            // 
+            // txtboxmonth
+            // 
+            this.txtboxmonth.Location = new System.Drawing.Point(367, 183);
+            this.txtboxmonth.Name = "txtboxmonth";
+            this.txtboxmonth.Size = new System.Drawing.Size(102, 22);
+            this.txtboxmonth.TabIndex = 1;
+            // 
+            // txtboxyear
+            // 
+            this.txtboxyear.Location = new System.Drawing.Point(367, 239);
+            this.txtboxyear.Name = "txtboxyear";
+            this.txtboxyear.Size = new System.Drawing.Size(102, 22);
+            this.txtboxyear.TabIndex = 2;
+            // 
+            // txtboxage
+            // 
+            this.txtboxage.BackColor = System.Drawing.Color.SkyBlue;
+            this.txtboxage.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtboxage.Font = new System.Drawing.Font("Trebuchet MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtboxage.Location = new System.Drawing.Point(176, 388);
+            this.txtboxage.Multiline = true;
+            this.txtboxage.Name = "txtboxage";
+            this.txtboxage.ReadOnly = true;
+            this.txtboxage.Size = new System.Drawing.Size(415, 57);
+            this.txtboxage.TabIndex = 7;
+            this.txtboxage.TabStop = false;
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.btnSubmit.Location = new System.Drawing.Point(332, 307);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(98, 46);
+            this.btnSubmit.TabIndex = 3;
+            this.btnSubmit.Text = "SUBMIT";
+            this.btnSubmit.UseVisualStyleBackColor = false;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // txtboxdate
+            // 
+            this.txtboxdate.Location = new System.Drawing.Point(367, 123);
+            this.txtboxdate.Name = "txtboxdate";
+            this.txtboxdate.Size = new System.Drawing.Size(102, 22);
+            this.txtboxdate.TabIndex = 0;
+            // 
+            // frmAgecalculator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(813, 475);
+            this.Controls.Add(this.txtboxdate);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.txtboxage);
+            this.Controls.Add(this.txtboxyear);
+            this.Controls.Add(this.txtboxmonth);
+            this.Controls.Add(this.lblBirthyear);
+            this.Controls.Add(this.lblBirthMontth);
+            this.Controls.Add(this.lbldate);
+            this.Name = "frmAgecalculator";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Age Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbldate;
+        private System.Windows.Forms.Label lblBirthMontth;
+        private System.Windows.Forms.Label lblBirthyear;
+        private System.Windows.Forms.TextBox txtboxmonth;
+        private System.Windows.Forms.TextBox txtboxyear;
+        private System.Windows.Forms.TextBox txtboxage;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.TextBox txtboxdate;
+    }
+}
+
---
+++ Age_calculator/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Age_calculator
+{
+    public partial class frmAgecalculator : Form
+    {
+        public frmAgecalculator()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            int[] month = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+            try
+            {
+                if (isValid())
+                {
+                    int birth_date = Convert.ToInt32(txtboxdate.Text);
+                    int birth_month = Convert.ToInt32(txtboxmonth.Text);
+                    int birth_year = Convert.ToInt32(txtboxyear.Text);
+                    DateTime current = DateTime.Today;
+                    var currentDate = current.ToString("dd");
+                    int current_date = Convert.ToInt32(currentDate);
+                    int current_month = current.Month;
+                    int current_year = current.Year;
+                    int calculated_date, calculated_month;
+                    if (current_date > birth_date)
+                    {
+                        current_date = current_date + month[birth_month - 1];
+                        current_month = current_month - 1;
+                    }
+                    if (birth_month > current_month)
+                    {
+                        current_year = current_year - 1;
+                        current_month = current_month + 12;
+                    }
+                    calculated_date = current_date - birth_date;
+                    calculated_month = current_month - birth_month;
+                    int calculated_year = current_year - birth_year;
+                    string age = "You are " + calculated_year + " years " + calculated_month + " months " + calculated_date + " days old";
+
+                    txtboxage.Text = age;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message,"Exception");
+            }
+        }
+            public bool isValid()
+            {
+            return isPresent(txtboxdate, "Enter your birthdate") &&
+                   isInt(txtboxdate, "Enter your birthdate") &&
+                   isPresent(txtboxmonth, "Enter your birthmonth") &&
+                   isInt(txtboxmonth, "Enter your birthmonth") &&
+                   isPresent(txtboxyear, "Enter your birthyear") &&
+                   isInt(txtboxyear, "Enter your birthyear");
+            }
+            public bool isPresent(TextBox txt,string s)
+            {
+            if(txt.Text == "")
+            {
+                MessageBox.Show(s + " is a required field. ", "Entry Error");
+                txt.Focus();
+                return false;
+            }
+            return true;
+            }
+            public bool isInt(TextBox txt,string s)
+        {
+            int number = 0;
+            if(Int32.TryParse(txt.Text, out number)){
+                return true;
+            }
+            else
+            {
+                MessageBox.Show(s + " must be an integer ", "Entry Error");
+                txt.Focus();
+                return false;
+            }
+        }
+        
+    }
+}
